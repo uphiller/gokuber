0c5a356208135fe1e7b64d5331862e25aeb9a088
uphiller <jason.uphiller@gmail.com>
2020-05-20 17:52:40 +0900

    keyclock 연동 추가

--- user/main.go
+++ user/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"github.com/coreos/go-oidc"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
 	"net/http"
 )
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	v1 := r.Group("/v1")
+	v1 := r.Group("/v1/user")
 	{
 		v1.GET("/health", health)
-		v1.GET("/user", user)
+		v1.GET("/login", login)
+		v1.GET("/callback", user)
 	}
 	return r
 }
@@ -24,6 +27,26 @@ func health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "ok",
 	})
+}
+
+func login(c *gin.Context) {
+
+	provider, err := oidc.NewProvider(c, "http://localhost:8080/auth/realms/master/account/")
+	if err != nil {
+		// handle error
+	}
+	oauth2Config := oauth2.Config{
+		ClientID:     "Gokubermanagement-user",
+		ClientSecret: "b53f3ca6-0559-4781-a9ee-a35cb0d7a64a",
+		RedirectURL:  "http://localhost:5000/user/callback",
+
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	c.Redirect(http.StatusMovedPermanently, oauth2Config.AuthCodeURL("abc"))
 }
 
 func user(c *gin.Context) {
--- user/main_test.go
+++ user/main_test.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
+	"encoding/json"
 	"testing"
+
+	// REST
+	"gopkg.in/resty.v1"
+
+	// Encoding
+	b64 "encoding/base64"
 )
 
+func getBasicAuthForClient(clientId string, clientSecret string) string {
+	var httpBasicAuth string
+	if len(clientId) > 0 && len(clientSecret) > 0 {
+		httpBasicAuth = b64.URLEncoding.EncodeToString([]byte(clientId + ":" + clientSecret))
+	}
+
+	return "Basic " + httpBasicAuth
+}
+
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
+	resp, err := resty.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetHeader("Authorization", getBasicAuthForClient("gokubermanagement-user", "b53f3ca6-0559-4781-a9ee-a35cb0d7a64a")).
+		SetFormData(map[string]string{
+			"grant_type": "password",
+			"username":   "tester",
+			"password":   "1234",
+		}).Post("http://localhost:8080/auth/realms/master/protocol/openid-connect/token")
+	if err != nil {
+		print(err)
+	}
+	var result map[string]interface{}
+	if err := json.Unmarshal(resp.Body(), &result); err != nil {
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/v1/user", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
+	}
+	print(result["access_token"])
 }
