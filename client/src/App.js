ee0327bcfa9657ea21f23a960ae00385bc3a5673
uphiller <jason.uphiller@gmail.com>
2020-05-25 17:55:35 +0900

    [front] add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import { renderRoutes } from 'react-router-config';
 import './App.scss';
 
@@ -12,13 +12,13 @@ class App extends Component {
 
     render() {
         return (
-            <HashRouter>
+            <BrowserRouter>
                 <React.Suspense fallback={loading()}>
                     <Switch>
                         <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
                     </Switch>
                 </React.Suspense>
-            </HashRouter>
+            </BrowserRouter>
         );
     }
 }
